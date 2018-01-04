77b0301b6f49d7862d3f0d4aadcde2576889221b
Арляпов Савелий <sava_arl@mail.ru>
2018-01-04 14:42:16 +0000

    Data to log

--- main.pl
+++ main.pl
@@ -1,15 +1,32 @@
 :- use_module(library(writef)).
 
-write_id(I) :- thread_self(X), writef('my id is %t\n', [X]), open('output', append, O), atom_concat(I, ' thread', R),writeln(O, R).
+% DEVEL
+
+write_id(Id) :-
+	atom_concat('My id is ', Id, Res),
+	write2log(Res).
+
 make_threads(N, []) :- N =< 0.
-make_threads(N, [Id|Ids]) :- N > 0, thread_create(go(N), Id, []), I is N - 1, make_threads(I, Ids).
+make_threads(N, [Id|Ids]) :- N > 0,
+	thread_create(go(N), Id, []),
+	I is N - 1,
+	make_threads(I, Ids).
 
+% MISC
 
-%misc
-clear_log :- write('Clear log!\n'),
-	(exists_file('output') -> delete_file('output'); true).
 go(X) :- with_mutex(mut,write_id(X)).
 
+write2log(Mes) :-
+	thread_self(TID),
+	open('project.log', append, LOGFILE),
+	get_time(Now), format_time(atom(Date), '%d %b %Y %T ', Now, posix),
+	atom_concat(Date, TID, Info),
+	atom_concat(' ', Mes, Line),
+	atom_concat(Info, Line, Res),
+	writeln(LOGFILE, Res).
+
+join([]).
+join([I|T]):-thread_join(I,_), join(T).
 
 run:-
 	mutex_create(mut),
@@ -18,21 +35,9 @@ run:-
 	join(Ids),
 	mutex_destroy(mut).
 
-join([]).
-join([I|T]):-thread_join(I,_), join(T).
-
-%initialization:-
-%	run,
-%	halt.
-
-:- run.
-:- halt.
-
-
-% ?- run.
-
 % GOAL
-%?- clear_log.
-%?- go(0).
-%?- make_threads(6).
-%?- halt.
+
+%:- write2log('Start main thread').
+:- run.
+%:- write2log('Stop main thread').
+:- halt.
