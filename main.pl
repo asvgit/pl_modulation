03878b3d572002de54be50f544a650147967fd49
Арляпов Савелий <sava_arl@mail.ru>
2018-01-23 01:03:26 +0800

    Add road map building

--- main.pl
+++ main.pl
@@ -23,23 +23,64 @@ do_loop_step(Step) :-
 
 % MISC ELEVATORS
 
-% append2map(Elev, Floor).
-% get_min(Distances, Elev) :- Elev is 0.
-% fill_dist(Dist, Distances) :- Dist is Distances.
+append2map(Elev, Floor) :-
+	atom_concat('elev_rmap_', Elev, MapName),
+	term_string(MapTerm, MapName),
+	nb_getval(MapTerm, Map),
+	append(Map, [Floor], MapWithFloor),
+	append(MapWithFloor, [-1], NewMap),
+	nb_setval(MapTerm, NewMap),
+	swritef(ElevLog, 'Append to road map \'%t\' floor \'%t\'', [NewMap, Floor]),
+	write2log(ElevLog).
 
-find_available_elev(Floor). % :-
-	% nb_getval(n_elevators, NElev),
-	% zero_list(NElev, Dist),
-	% fill_dist(Dist, Distances),
-	% swritef(ElevLog, 'Current distances are \'%t\'', [Distances]),
-	% write2log(ElevLog),
-	% get_min(Distances, Elev),
-	% append2map(Elev, Floor).
+get_min_dist_id([], 100500, Id) :- nb_getval(n_elevators, Id).
+get_min_dist_id([HDist | TDist], Res, Id) :-
+	get_min_dist_id(TDist, Min, MinId),
+	(Min >= HDist ->
+		Res is HDist,
+		Id is MinId - 1
+	;
+		Res is Min,
+		Id is MinId
+	).
+
+get_dist(Pos, [], Floor, Res) :- Res is abs(Pos - Floor).
+get_dist(Pos, [H | T], Floor, Res) :-
+	(H = -1 ->
+		get_dist(Pos, T, Floor, Dist),
+		Res is Dist + 1
+	;
+		get_dist(H, T, Floor, Dist),
+		Res is Dist + abs(Pos - H)
+	).
+
+fill_dist(0, _, Dist, Dist).
+fill_dist(Ind, Floor, Dist, Res) :-
+	NextInd is Ind - 1,
+	nb_getval(elevators_floors, ElevFloors),
+	get_elem(ElevFloors, NextInd, 0, ElevPos),
+	atom_concat('elev_rmap_', NextInd, MapName),
+	term_string(MapTerm, MapName),
+	nb_getval(MapTerm, Map),
+	get_dist(ElevPos, Map, Floor, D),
+	set_elem(Dist, NextInd, 0, D, NewDist),
+	fill_dist(NextInd, Floor, NewDist, Res).
+
+find_available_elev(Floor) :-
+	nb_getval(n_elevators, NElev),
+	zero_list(NElev, Dist),
+	fill_dist(NElev, Floor, Dist, Distances),
+	swritef(ElevDistLog, 'Current distances are \'%t\'', [Distances]),
+	write2log(ElevDistLog),
+	get_min_dist_id(Distances, MinDist, Elev),
+	swritef(ElevMinDistLog, 'Current min dist \'%t\' with id \'%t\'', [MinDist, Elev]),
+	write2log(ElevMinDistLog),
+	append2map(Elev, Floor).
 
 find_in_list([], _, Res) :- Res = false.
 find_in_list([H | T], Val, Res) :-
 	(H = Val ->
-		Res is true
+		Res = true
 	;
 		find_in_list(T, Val, Res)
 	).
@@ -48,12 +89,12 @@ find_in_elev_lists(_, Ind, Res) :- Ind =< 0, Res = false.
 find_in_elev_lists(Floor, Ind, Res) :-
 	Ind >= 0,
 	NextInd is Ind - 1,
-	atom_concat('elev_map_', NextInd, ListName),
+	atom_concat('elev_rmap_', NextInd, ListName),
 	term_string(ListTerm, ListName),
-	nb_setval(ListTerm, List),
+	nb_getval(ListTerm, List),
 	find_in_list(List, Floor, FRes),
 	(FRes ->
-		Res is true
+		Res = true
 	;
 		find_in_elev_lists(Floor, NextInd, Res)
 	).
@@ -93,7 +134,7 @@ manage_people_appear(_, _, []).
 manage_people_appear(Step, Id, [H | T]) :-
 	(Step = H ->
 		get_people_elem(people_floors, Id, Floor),
-		swritef(AppearLog, 'A man appears with id \'%t\' on floor with id %t', [Id, Floor]),
+		swritef(AppearLog, 'A man appears with id \'%t\' on floor with id \'%t\'', [Id, Floor]),
 		write2log(AppearLog),
 		set_people_elem(people_states, Id, 1),
 		elev_call(Floor)
@@ -238,7 +279,7 @@ init_elevators :-
 	write2log('Init elevators proc'),
 	nb_getval(n_elevators, N),
 	zero_list(N, List),
-	nb_setval(elevators_waiting, List),
+	nb_setval(elevators_floors, List),
 	swritef(ElevLog, 'Init elevators floors \'%t\'', [List]),
 	write2log(ElevLog),
 	init_elev_lists.
